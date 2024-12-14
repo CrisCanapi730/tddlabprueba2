d341efe823ed50f1868170735b5ef73c5d571aae
Cristian Canapi <cristiancanapi730@gmail.com>
2024-12-14 00:52:50 -0400

    Se arreglaron problemas

--- src/sumador.js
+++ src/sumador.js
@@ -11,12 +11,12 @@ export function fizzbuzz3(a) {
 export function fizzbuzz4(a) {
   return 1;
 }
-export function fizzbuzz2(a) {
+export function fizzbuzz5(a) {
   return a;
 }
-export function fizzbuzz3(a) {
+export function fizzbuzz6(a) {
   return 1;
 }
-export function fizzbuzz4(a) {
+export function fizzbuzz7(a) {
   return 1;
 }
